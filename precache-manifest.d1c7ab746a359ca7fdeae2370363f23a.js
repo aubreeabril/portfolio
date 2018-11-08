52a7ceb402711038193d2ca57fc4fa4564fce8f8
aubreeabril <aubree.abril@gmail.com>
2018-11-07 22:25:01 -0500

    Updates

--- precache-manifest.d1c7ab746a359ca7fdeae2370363f23a.js
+++ precache-manifest.d1c7ab746a359ca7fdeae2370363f23a.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
   },
   {
-    "revision": "cc0f06c73f5100b9f2a8",
-    "url": "/portfolio/static/js/main.cc0f06c7.chunk.js"
+    "revision": "349e68713bcd347c04fd",
+    "url": "/portfolio/static/js/main.349e6871.chunk.js"
   },
   {
     "revision": "9a0fbbdae75f42fd5b35",
     "url": "/portfolio/static/js/1.9a0fbbda.chunk.js"
   },
   {
-    "revision": "cc0f06c73f5100b9f2a8",
+    "revision": "349e68713bcd347c04fd",
     "url": "/portfolio/static/css/main.ad555276.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/portfolio/static/css/1.dbd0fcaf.chunk.css"
   },
   {
-    "revision": "51d9a09466238b24585feeab40b95352",
+    "revision": "546c69d6f55e135a3633967bee8bed8c",
     "url": "/portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.5191c362ba003dfd266690117784db09.js"
+  "/portfolio/precache-manifest.d1c7ab746a359ca7fdeae2370363f23a.js"
 );
 
 workbox.clientsClaim();
